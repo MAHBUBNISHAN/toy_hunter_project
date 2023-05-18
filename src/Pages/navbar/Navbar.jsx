bd629499d60caa4427bfa6106a3c32b1785f0054
Mahbubnishan <mahbubnishan6@gmail.com>
2023-05-18 18:22:05 +0600

    commit with nab

--- src/Pages/navbar/Navbar.jsx
+++ src/Pages/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
-            <h2> this is navbar</h2>
-        </div>
+            
+        <div className="navbar bg-neutral text-neutral-content flex justify-between ">
+            <div>
+
+    <a className="btn btn-ghost normal-case text-xl bg-fuchsia-300 mx-2">Chef Hunter</a>
+    <img src="" alt="" />
+            </div>
+<div className='mx-3'>
+<Link className="btn btn-ghost normal-case text-xl"  to="/">Home</Link>
+<Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
+<Link className="btn btn-ghost normal-case text-xl" to="/register">Register</Link>
+<Link className="btn btn-ghost normal-case text-xl" to="/blog">Blog</Link>
+
+</div>
+</div>
+    </div>
     );
 };
 

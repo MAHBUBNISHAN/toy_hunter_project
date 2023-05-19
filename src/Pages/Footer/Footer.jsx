611b3dd0d83ec589b19d8c06bbb95b6e6f561dc8
Mahbubnishan <mahbubnishan6@gmail.com>
2023-05-19 15:33:41 +0600

    commite with footer

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 const Footer = () => {
     return (
+        <footer className="footer p-10  bg-base-200 text-base-content">
         <div>
-            <h2 className='text-6xl'> This is footer</h2>
+          <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"></svg>
+          <img src="" alt="" />
+          <p>Toy Summation Industries Ltd.</p>
+        </div> 
+        <div>
+          <span className="footer-title">Services Media Link</span> 
+          <a className="link link-hover">Facebook</a> 
+          <a className="link link-hover">e-mail</a> 
+          <a className="link link-hover">youtube</a> 
+          <a className="link link-hover">Linkdin</a>
+        </div> 
+        <div>
+          <span className="footer-title">Company</span> 
+          <a className="link link-hover">About us</a> 
+          <a className="link link-hover">Contact</a> 
+          <a className="link link-hover">Jobs</a> 
+          <a className="link link-hover">Press kit</a>
+        </div> 
+        <div>
+          <span className="footer-title">Legal</span> 
+          <a className="link link-hover">Terms of use</a> 
+          <a className="link link-hover">Privacy policy</a> 
         </div>
+        
+      </footer>
     );
 };
 
-export default Footer;<h2> This is footer</h2>
+export default Footer;
